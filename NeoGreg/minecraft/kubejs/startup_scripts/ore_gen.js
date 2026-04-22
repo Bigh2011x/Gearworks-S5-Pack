a148f51aeb8f62f7392764933a47430601f7bdcf
Bigh2011x <harveyovington@outlook.com>
2026-04-22 20:01:37 +0100

    v0.1.0

--- NeoGreg/minecraft/kubejs/startup_scripts/ore_gen.js
+++
@@ -1,8 +0,0 @@
-WorldgenEvents.remove(event => {
-    event.removeOres(ores => {
-        ores.blocks = [
-            'create:zinc_ore',
-            'create:deepslate_zinc_ore'
-        ]
-        })
-})
